93b8f352331f61f317a5b4b6352d1d391a3ed5d6
PabloUribeUCB <pablo.uribe@ucb.edu.bo>
2025-12-17 20:08:32 -0400

    Update Proyecto.cpp

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -12,8 +12,9 @@ void MenuOpciones(string archivoCarreras, string archivoEstudiantes, string arch
 void NuevoPrestamo(string archivoprestamos, string archivoEstudiantes);
 void agregarlibro(string archivoLibros);
 void listadoLibros(string archivoLibros);
-//void listadoLibrosImpresora(string archivoLibros);
-//void listadoEstudiantes(string archivoEstudiantes, string archivoCarreras);
+void listadoLibrosImpresora(string archivoLibros);
+void ReportePrestamosActivos(string archivoprestamos);
+void ReporteLibroMasPrestado(string archivoprestamos, string archivoLibros);
 
 int main()
 {
@@ -21,14 +22,17 @@ int main()
     string archivoEstudiantes = "estudiantes.bin";
     string archivoprestamos = "prestamos.bin";
     string archivoLibros = "libros.bin";
+
     MenuOpciones(archivoCarreras, archivoEstudiantes, archivoprestamos, archivoLibros);
     return 0;
 }
 
-void MenuOpciones(string archivoCarreras, string archivoEstudiantes, string archivoprestamos, string archivoLibros){
+void MenuOpciones(string archivoCarreras, string archivoEstudiantes, string archivoprestamos, string archivoLibros)
+{
     int opcion = 0;
     int subopcion = 0;
     int subopcion2 = 0;
+
     do
     {
         system("cls");
@@ -42,138 +46,149 @@ void MenuOpciones(string archivoCarreras, string archivoEstudiantes, string arch
         cout << "0. Salir" << endl;
         cout << "Seleccione una opcion: ";
         cin >> opcion;
+
         switch (opcion)
         {
-            case 1:
-                NuevoPrestamo(archivoprestamos, archivoEstudiantes);
-                system("pause");          
-                break;
-            case 2:
-                do
+        case 1:
+            NuevoPrestamo(archivoprestamos, archivoEstudiantes);
+            system("pause");
+            break;
+
+        case 2:
+            do
+            {
+                system("cls");
+                cout << "Organizar Inventario" << endl;
+                cout << "================================" << endl;
+                cout << "1. Agregrar Libro" << endl;
+                cout << "2. Listado Libros" << endl;
+                cout << "3. Listado Libros (Impresora)" << endl;
+                cout << "0. Salir" << endl;
+                cout << "Seleccione una opcion: ";
+                cin >> subopcion;
+
+                switch (subopcion)
                 {
-                    system("cls");
-                    cout << "Organizar Inventario" << endl;
-                    cout << "================================" << endl;
-                    cout << "1. Agregrar Libro" << endl;
-                    cout << "2. Listado Libros" << endl;
-                    cout << "3. Listado Libros (Impresora)" << endl;
-                    cout << "0. Salir" << endl;
-                    cout << "Seleccione una opcion: ";
-                    cin >> subopcion;
-                    switch (subopcion)
-                    {
-                        case 1:
-                            agregarlibro(archivoLibros);
-                            system("pause");
-                            break;
-                        case 2:
-                            listadoLibros(archivoLibros);
-                            system("pause");
-                            break;
-                        case 3:
-                            //listadoLibrosImpresora(archivoLibros);
-                            system("pause");
-                            break;    
-                        default:
-                            if (subopcion != 0)
-                            {
-                                cout << "Opcion no valida." << endl;
-                                system("pause");
-                            }
-                            break;
-                    }
-                } while (subopcion != 0);            
-                break;
-            case 3:
-                do
-                {
-                    system("cls");
-                    cout << "MENU CARRERAS" << endl;
-                    cout << "=============" << endl;
-                    cout << "1. Crear Carrera" << endl;
-                    cout << "2. Listado Carreras" << endl;
-                    cout << "0. Salir" << endl;
-                    cout << "Seleccione una opcion: ";
-                    cin >> subopcion;
-                    switch (subopcion)
-                    {
-                        case 1:
-                            //CrearCarrera(archivoCarreras);
-                            system("pause");
-                            break;
-                        case 2:
-                            //ListadoCarreras(archivoCarreras);
-                            system("pause");
-                            break;
-                        default:
-                            if (subopcion != 0)
-                            {
-                                cout << "Opcion no valida." << endl;
-                                system("pause");
-                            }
-                            break;
-                    }
-                } while (subopcion != 0); 
-                break;
-            case 4:
-                //NuevoEstudiante(archivoEstudiantes);
-                system("pause");
-                break;
-            case 5:
-                do
-                {
-                    system("cls");
-                    cout << "MENU REPORTES (Impresora)" << endl;
-                    cout << "=============" << endl;
-                    cout << "1. Libro mas prestado" << endl;
-                    cout << "2. Estudiantes con prestamos activos" << endl;
-                    cout << "3. Libro menos prestado" << endl;
-                    cout << "4. Estudiantes morosos" << endl;
-                    cout << "5. Prestamos por carrera" << endl;
-                    cout << "0. Salir" << endl;
-                    cout << "Seleccione una opcion: ";
-                    cin >> subopcion2;
-                    switch (subopcion2)
-                    {
-                        case 1:
-                            //listadoEstudiantes(archivoEstudiantes, archivoCarreras);
-                            system("pause");
-                            break;
-                        case 2:
-                            //listadoEstudiantes(archivoEstudiantes, archivoCarreras);
-                            system("pause");
-                            break;
-                        case 3:
-                            //listadoEstudiantes(archivoEstudiantes, archivoCarreras);
-                            system("pause");
-                            break;
-                        case 4:
-                            //listadoEstudiantes(archivoEstudiantes, archivoCarreras);
-                            system("pause");
-                            break;
-                        default:
-                            if (subopcion2 != 0)
-                            {
-                                cout << "Opcion no valida." << endl;
-                                system("pause");
-                            }
-                            break;
-                    }
-                } while (subopcion2 != 0);
-            
-            default:
-                if (opcion != 0)
-                {
-                    cout << "Opcion no valida." << endl;
+                case 1:
+                    agregarlibro(archivoLibros);
                     system("pause");
+                    break;
+                case 2:
+                    listadoLibros(archivoLibros);
+                    system("pause");
+                    break;
+                case 3:
+                    listadoLibrosImpresora(archivoLibros);
+                    system("pause");
+                    break;
+                default:
+                    if (subopcion != 0)
+                    {
+                        cout << "Opcion no valida." << endl;
+                        system("pause");
+                    }
+                    break;
                 }
-                break;
+            } while (subopcion != 0);
+            break;
+
+        case 3:
+            do
+            {
+                system("cls");
+                cout << "MENU CARRERAS" << endl;
+                cout << "=============" << endl;
+                cout << "1. Crear Carrera" << endl;
+                cout << "2. Listado Carreras" << endl;
+                cout << "0. Salir" << endl;
+                cout << "Seleccione una opcion: ";
+                cin >> subopcion;
+
+                switch (subopcion)
+                {
+                case 1:
+                    //CrearCarrera(archivoCarreras);
+                    system("pause");
+                    break;
+                case 2:
+                    //ListadoCarreras(archivoCarreras);
+                    system("pause");
+                    break;
+                default:
+                    if (subopcion != 0)
+                    {
+                        cout << "Opcion no valida." << endl;
+                        system("pause");
+                    }
+                    break;
+                }
+            } while (subopcion != 0);
+            break;
+
+        case 4:
+            //NuevoEstudiante(archivoEstudiantes);
+            system("pause");
+            break;
+
+        case 5:
+            do
+            {
+                system("cls");
+                cout << "MENU REPORTES (Impresora)" << endl;
+                cout << "=============" << endl;
+                cout << "1. Libro mas prestado" << endl;
+                cout << "2. Estudiantes con prestamos activos" << endl;
+                cout << "3. Libro menos prestado" << endl;
+                cout << "4. Estudiantes morosos" << endl;
+                cout << "5. Prestamos por carrera" << endl;
+                cout << "0. Salir" << endl;
+                cout << "Seleccione una opcion: ";
+                cin >> subopcion2;
+
+                switch (subopcion2)
+                {
+                case 1:
+                    ReporteLibroMasPrestado(archivoprestamos, archivoLibros);
+                    system("pause");
+                    break;
+                case 2:
+                    ReportePrestamosActivos(archivoprestamos);
+                    system("pause");
+                    break;
+                case 3:
+                    //listadoEstudiantes(archivoEstudiantes, archivoCarreras);
+                    system("pause");
+                    break;
+                case 4:
+                    //listadoEstudiantes(archivoEstudiantes, archivoCarreras);
+                    system("pause");
+                    break;
+                default:
+                    if (subopcion2 != 0)
+                    {
+                        cout << "Opcion no valida." << endl;
+                        system("pause");
+                    }
+                    break;
+                }
+            } while (subopcion2 != 0);
+            break;
+
+        default:
+            if (opcion != 0)
+            {
+                cout << "Opcion no valida." << endl;
+                system("pause");
+            }
+            break;
         }
     } while (opcion != 0);
+
     system("cls");
 }
 
-/*void CrearCarrera(string archivoCarreras)
+/*
+void CrearCarrera(string archivoCarreras)
 {
     Carrera _carrera;
     ofstream archivo;
@@ -219,4 +234,5 @@ void ListadoCarreras(string archivoCarreras)
         cout << "No se pudo abrir el archivo." << endl;
     }
     archivo.close();
-}       */
+}
+*/
